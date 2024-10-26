0ffedecd0b663d983b3d12de47f03def40b697c1
Marcia Castro <marcas4@gmail.com>
2024-10-25 20:11:07 -0500

    Primer commit, inicializando el proyecto de apirest_mascotas

---
+++ src/test/java/co/com/marcas/apirest/ApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.marcas.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
